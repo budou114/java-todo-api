3e84558ab5f1e44f9064a5d91000f884b3158f26
budou114 <58349030+budou114@users.noreply.github.com>
2024-01-20 14:45:38 +0900

    43. MyBatis を使ってデータベースにアクセスしよう

--- src/main/java/com/example/todoapi/repository/sample/SampleRepository.java
+++ src/main/java/com/example/todoapi/repository/sample/SampleRepository.java
@@ -1,10 +1,10 @@
 package com.example.todoapi.repository.sample;
 
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
-@Repository
-public class SampleRepository {
-    public SampleRecord select() {
-        return new SampleRecord("Hello world!");
-    }
+@Mapper
+public interface SampleRepository {
+    @Select("SELECT content FROM samples ORDER BY id LIMIT 1")
+    SampleRecord select();
 }
