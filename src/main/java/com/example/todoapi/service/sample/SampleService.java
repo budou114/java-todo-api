5cb62355cbb2efd4dfdbb41da4152b24eac61ee7
budou114 <58349030+budou114@users.noreply.github.com>
2024-01-20 10:46:23 +0900

    39. Repository と Record を実装しよう

--- src/main/java/com/example/todoapi/service/sample/SampleService.java
+++ src/main/java/com/example/todoapi/service/sample/SampleService.java
@@ -1,10 +1,15 @@
 package com.example.todoapi.service.sample;
 
+import com.example.todoapi.repository.sample.SampleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SampleService {
+    private final SampleRepository repository;
     public SampleEntity find() {
-        return new SampleEntity("Hello");
+        var record = repository.select();
+        return new SampleEntity(record.getContent());
     }
 }
