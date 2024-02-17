2d9da2f335411fad6135d852d2fb1aa86e191088
budou114 <58349030+budou114@users.noreply.github.com>
2024-02-17 13:03:56 +0900

    102. 400 Bad Request：リファクタリング

--- src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
+++ src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
@@ -2,6 +2,7 @@ package com.example.todoapi.controller.advice;
 
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.InvalidParam;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ElementKind;
 import org.springframework.validation.FieldError;
@@ -36,23 +37,29 @@ public class BadRequestErrorCreator {
     }
 
     public static BadRequestError from(ConstraintViolationException ex) {
-        var invalidParamList = ex.getConstraintViolations()
-                .stream()
-                .map(violation -> {
-                    var parameterOpt = StreamSupport.stream(violation.getPropertyPath().spliterator(), false)
-                            .filter(node -> node.getKind().equals(ElementKind.PARAMETER))
-                            .findFirst();
-                    var invalidParam = new InvalidParam();
-                    parameterOpt.ifPresent(p -> invalidParam.setName(p.getName()));
-
-                    invalidParam.setReason(violation.getMessage());
-                    return invalidParam;
-                })
-                .collect(Collectors.toList());
+        var invalidParamList = createInvalidParamList(ex);
 
         var error = new BadRequestError();
         error.setInvalidParams(invalidParamList);
 
         return error;
+    }
+
+    private static List<InvalidParam> createInvalidParamList(ConstraintViolationException ex) {
+        return ex.getConstraintViolations()
+                .stream()
+                .map(BadRequestErrorCreator::createInvalidParam)
+                .collect(Collectors.toList());
+    }
+
+    private static InvalidParam createInvalidParam(ConstraintViolation<?> violation) {
+        var parameterOpt = StreamSupport.stream(violation.getPropertyPath().spliterator(), false)
+                .filter(node -> node.getKind().equals(ElementKind.PARAMETER))
+                .findFirst();
+        var invalidParam = new InvalidParam();
+        parameterOpt.ifPresent(p -> invalidParam.setName(p.getName()));
+
+        invalidParam.setReason(violation.getMessage());
+        return invalidParam;
     }
 }
