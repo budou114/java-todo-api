afb4fc2d2e84d4e77967d517f0121bd1d906efc5
budou114 <58349030+budou114@users.noreply.github.com>
2024-02-17 11:45:09 +0900

    100. 400 Bad Request：例外ハンドラー実装

--- src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
+++ src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
@@ -2,6 +2,7 @@ package com.example.todoapi.controller.advice;
 
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.InvalidParam;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
@@ -30,5 +31,9 @@ public class BadRequestErrorCreator {
         invalidParam.setName(fieldError.getField());
         invalidParam.setReason(fieldError.getDefaultMessage());
         return invalidParam;
+    }
+
+    public static BadRequestError from(ConstraintViolationException ex) {
+        return new BadRequestError();
     }
 }
